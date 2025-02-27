e9d5def594b9f61534bfd384a812bd315767cecb
João Pedro <joaope.espindola2005@gmail.com>
2025-02-27 10:33:09 -0300

    feat: Add CursoRepository with search functionality

---
+++ src/main/java/br/com/joaopedro/desafio_api/repository/CursoRepository.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.desafio_api.repository;
+
+import br.com.joaopedro.desafio_api.entities.Curso;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, UUID> {
+ List<Curso> findByNameContainingIgnoreCase (String name, String category);
+ 
+} 
