dcf07e297498edcb85784da1f38cd8b5c89abf6e
João Pedro <joaope.espindola2005@gmail.com>
2025-04-02 17:17:01 -0300

    refactor: change endpoint from PATCH to POST to resolve Spring conflict

--- src/main/java/br/com/joaopedro/desafio_api/controller/CursoController.java
+++ src/main/java/br/com/joaopedro/desafio_api/controller/CursoController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -82,7 +81,7 @@ public class CursoController {
    }
   }
 
-  @PatchMapping("/{id}/active")
+  @PostMapping("/{id}/active")
   public ResponseEntity<String> checkCurso(@PathVariable UUID id) {
    try {
     cursoUseCase.checkCurso(id);
